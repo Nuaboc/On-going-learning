cbb3a5d3b13f42a16f92ae44f2aa64c89405fdf7
Gabriel Martinez <31278920+Nuaboc@users.noreply.github.com>
2020-06-28 18:18:10 -0400

    adding base code for exercises #4 and #5

---
+++ Python/Doing Math With Python/doing_math_chapt1_exercise4.py
@@ -0,0 +1,23 @@
+# Doing Math With Python
+# Chapter 1 Working With Numbers
+# Programming Challenges
+
+# Fraction Calculator
+
+"""
+Fraction operations
+"""
+
+from fractions import Fraction
+
+
+def add(a, b):
+    print('Result of Addition: {0}'.format(a+b))
+
+
+if __name__ == '__main__':
+    a = Fraction(input('Enter first fraction: '))
+    b = Fraction(input('Enter second fraction: '))
+    op = input('Operation to perform - Add, Subtract, Divide, Multiply: ')
+    if op == 'Add':
+        add(a,b)
---
+++ Python/Doing Math With Python/doing_math_chapt1_exercise5.py
@@ -0,0 +1,22 @@
+# Doing Math With Python
+# Chapter 1 Working With Numbers
+# Programming Challenges
+
+# Give Exit Power to the User
+
+
+"""
+Run until exit layout
+"""
+
+
+def fun():
+    print('I am in an endless loop')
+
+
+if __name__ == '__main__':
+    while True:
+        fun()
+        answer = input('Do you want to exit? (y) for yes ')
+        if answer == 'y':
+            break
