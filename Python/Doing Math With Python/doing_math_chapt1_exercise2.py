75fe848dfddcf91811f732e30ca284688fc9fe1a
Gabriel Martinez <31278920+Nuaboc@users.noreply.github.com>
2020-06-28 21:14:02 -0400

    Exercise 2 done.

--- Python/Doing Math With Python/doing_math_chapt1_exercise2.py
+++ Python/Doing Math With Python/doing_math_chapt1_exercise2.py
@@ -5,12 +5,19 @@
 # Enhanced Multiplication Table Generator
 
 
-def multi_table(a):
+def multi_table(x, y):
+    """By giving 2 arguments, this functions return a table with the quantity of multiples given."""
+    y += 1
 
-    for i in range(1, 11):
-        print('{0} x {1} = {2}'.format(a, i, a*i))
+    for i in range(1, y):
+        print('{0} x {1} = {2}'.format(x, i, x*i))
 
 
 if __name__ == '__main__':
-    a = input('Enter a number: ')
-    multi_table(int(a))
+    print('Which table would you like to see?')
+    num = input('Enter a number: ')
+    
+    # Ask up to how many multiples
+    print("Up to how many multiples of " + num + "?")
+    how_many = input('Enter a number: ')
+    multi_table(int(num), int(how_many))
