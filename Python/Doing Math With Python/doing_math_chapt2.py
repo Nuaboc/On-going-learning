da3f37a979a4b7edc265a6d8eb56164a7430ccf1
Gabriel Martinez <31278920+Nuaboc@users.noreply.github.com>
2020-07-08 22:51:32 -0400

    Entering Chapter 3
    Have a problem with importing 'kiwisolver' through the chapter 2.

--- Python/Doing Math With Python/doing_math_chapt2.py
+++ Python/Doing Math With Python/doing_math_chapt2.py
@@ -1,7 +1,16 @@
 # Doing Math With Python
 # Chapter 2 Visualizing Data With Graphs
 
+import matplotlib.pyplot as plt
+
 list0ne = [1, 2, 3]
 
 for x, y in enumerate(list0ne):
     print(x, y)
+
+x_nums = [1, 2, 3]
+y_nums = [4, 5, 6]
+
+plt.plot(x_nums, y_nums)
+
+plt.show()
