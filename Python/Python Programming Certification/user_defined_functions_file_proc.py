732acf6347c80fe1466c60fb0e40852decaf15ce
Gabriel Martinez <31278920+Nuaboc@users.noreply.github.com>
2020-07-17 11:55:37 -0400

    .

--- Python/Python Programming Certification/user_defined_functions_file_proc.py
+++ Python/Python Programming Certification/user_defined_functions_file_proc.py
@@ -1,18 +1,19 @@
-"""  
-A module to roll two dice
-
-Author: Walker M. White
-Date: January 20, 2019
-"""
 import random
-first = input('Type the first number: ')
-last = input('Type the last number: ')
-first = int(first)
-last = int(last)
-print('Choosing two numbers between '+str(first)+' and '+str(last)+'.')
 
-# Generate two numbers, add, and print the result
-num1 = random.randint(first,last)
-num2 = random.randint(first,last)
-thesum = num1+num2
-print('The sum is '+str(thesum)+'.')
+
+def rollem(first, last):
+    """
+    Returns the sum of two random numbers.
+
+    The numbers generated are between first and last (inclusive).
+
+    Example: rollem(1,6) can return any value between 2 and 12.
+
+    Parameter first: The lowest possible number
+    Precondition: first is an integer
+
+    Parameter last: The greatest possible number
+    Precondition: last is an integer, last >= first
+    """
+
+    return random.randint(first, last) + random.randint(first, last)
