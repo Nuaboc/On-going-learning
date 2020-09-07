500ce7f2564494126a135a2624335506ab0b906b
Gabriel Martinez <31278920+Nuaboc@users.noreply.github.com>
2020-09-07 09:32:23 -0400

    ...

--- Python/Python Programming Certification/Programming Flow/exerc6/test.py
+++ Python/Python Programming Certification/Programming Flow/exerc6/test.py
@@ -15,45 +15,45 @@ def test_first_vowel():
     print('Testing first_vowel()')
     # No vowels
     result = funcs.first_vowel('grrm')
-    introcs.assert_equals(-1,result)
+    introcs.assert_equals(-1, result)
 
     # Letter a
     result = funcs.first_vowel('pat')
-    introcs.assert_equals(1,result)
+    introcs.assert_equals(1, result)
 
     # Letter e
     result = funcs.first_vowel('step')
-    introcs.assert_equals(2,result)
+    introcs.assert_equals(2, result)
 
     # Letter i
     result = funcs.first_vowel('strip')
-    introcs.assert_equals(3,result)
+    introcs.assert_equals(3, result)
 
     # Letter o
     result = funcs.first_vowel('stop')
-    introcs.assert_equals(2,result)
+    introcs.assert_equals(2, result)
 
     # Letter u
     result = funcs.first_vowel('truck')
-    introcs.assert_equals(2,result)
+    introcs.assert_equals(2, result)
 
     # Letter y, not a vowel
     result = funcs.first_vowel('ygglx')
-    introcs.assert_equals(-1,result)
+    introcs.assert_equals(-1, result)
 
     # Letter y as vowel
     result = funcs.first_vowel('sky')
-    introcs.assert_equals(2,result)
+    introcs.assert_equals(2, result)
 
     # Various multi-vowel combinations
     result = funcs.first_vowel('apple')
-    introcs.assert_equals(0,result)
+    introcs.assert_equals(0, result)
 
     result = funcs.first_vowel('sleep')
-    introcs.assert_equals(2,result)
+    introcs.assert_equals(2, result)
 
     result = funcs.first_vowel('year')
-    introcs.assert_equals(1,result)
+    introcs.assert_equals(1, result)
 
     # Feel free to add more
 
@@ -65,7 +65,6 @@ def test_pigify():
     print('Testing pigify()')
 
     # Put your tests here
-
     result = funcs.pigify('ask')
     introcs.assert_equals('askhay', result)
 
@@ -89,7 +88,6 @@ def test_pigify():
 
     result = funcs.pigify('ssssh')
     introcs.assert_equals('sssshay', result)
-
 
 
 test_first_vowel()
