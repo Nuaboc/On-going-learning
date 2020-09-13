8bf8d1f22097051ffdf3f41aca1212e63ffc7d42
Gabriel Martinez <31278920+Nuaboc@users.noreply.github.com>
2020-09-13 02:00:21 -0400

    study session...

---
+++ Python/Python Programming Certification/Controlling Programming Flow/For-Loops/excer3/funcs.py
@@ -0,0 +1,44 @@
+"""
+Module with range-based for-loop functions.
+
+Author: Gabriel Martinez
+Date: September 12, 2020
+"""
+
+
+def factorial(n):
+    """
+    Returns n! = n * (n-1) * (n-2) ... * 1
+
+    0! is 1.  Factorial is undefined for integers < 0.
+
+    Examples:
+        factorial(0) returns 1
+        factorial(2) returns 2
+        factorial(3) returns 6
+        factorial(5) returns 120
+
+    Parameter n: The integer for the factorial
+    Precondition: n is an int >= 0
+    """
+    z = 0
+
+    for i in range(n):
+        z += i * z
+
+    return z
+
+
+def revrange(a,b):
+    """
+    Returns the tuple (b-1, b-2, ..., a)
+
+    Note that this tuple is the reverse of tuple(range(a,b))
+
+    Parameter a: the "start" of the range
+    Precondition: a is an int <= b
+
+    Parameter b: the "end" of the range
+    Precondition: b is an int >= a
+    """
+    pass
