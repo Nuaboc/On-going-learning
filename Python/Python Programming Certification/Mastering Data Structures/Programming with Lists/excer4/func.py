19860ff4661abaa023d8dab10249ad4b6b9367f2
Gabriel Martinez <31278920+Nuaboc@users.noreply.github.com>
2020-09-26 12:51:41 -0400

    study session...

---
+++ Python/Python Programming Certification/Mastering Data Structures/Programming with Lists/excer4/func.py
@@ -0,0 +1,43 @@
+"""
+Module to demonstrate tuple expansion.
+
+Author: Gabriel Martinez
+Date: September 26, 2020
+"""
+
+
+def avg(*args):   # The parameter is MISSING.  Add it back.
+    """
+    Returns average of all of arguments (passed via tuple expansion)
+
+    Remember that the average of a list of arguments is the sum of all of the elements
+    divided by the number of elements.
+
+    Examples:
+        avg(1.0, 2.0, 3.0) returns 2.0
+        avg(1.0, 1.0, 3.0, 5.0) returns 2.5
+
+    Parameter args: the function arguments
+    Precondition: args are all numbers (int or float)
+    """
+    average = int
+    x = 0
+
+    for i in args:
+        x += i
+        #print(x)
+
+    try:
+        average = x / len(args)
+
+    except ZeroDivisionError:
+        return 0
+
+    return average
+
+
+a = []
+for i in range(10, 20):
+    a.append(i)
+print(a)
+avg(a)
