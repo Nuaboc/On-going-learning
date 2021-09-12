f3bc49de3ba1d24bbc8f0b767d946b8239baeda6
Gabriel <sagamubriel@gmail.com>
2021-09-12 11:58:14 -0400

    learning...

---
+++ Swift/Udemy iOS & Swift Dev/Development/I am Fckg Rich/I am Fckg Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Fckg Rich
+//
+//  Created by Gabriel Martínez on 9/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
